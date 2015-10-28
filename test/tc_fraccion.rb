2ce4d41e5a4a50445676ea360bf3f8839022724f
alu0100505092 <alu0100505092@ull.edu.es>
2015-10-28 12:22:28 +0000

    Creado test de la division

--- test/tc_fraccion.rb
+++ test/tc_fraccion.rb
@@ -2,13 +2,14 @@ require '../lib/fraccion.rb'
 require 'test/unit'
 
 class TestFraccion < Test::Unit::TestCase
-    attr_reader :f1, :f2, :rest, :mult   
+    attr_reader :f1, :f2, :rest, :mult, :div  
     
     def setup 
         @f1 = Fraccion.new(3,2)
         @f2 = Fraccion.new(7,5)
         @rest = @f2-@f1
         @mult = @f2*@f1
+        @div = @f1/@f2
         
     end 
     
@@ -22,9 +23,12 @@ class TestFraccion < Test::Unit::TestCase
     
     def test_multiplicacion
         assert_equal(mult.num, 21)
+        assert_equal(mult.denum, 10)
     end
     
     def test_division
+        assert_equal(div.num, 15)
+        assert_equal(div.denum, 14)
     end
     
 end    
