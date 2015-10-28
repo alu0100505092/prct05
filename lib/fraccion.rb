6b9b66e28a554da28e2f5d711a804edd3569b6cd
alu0100505092 <alu0100505092@ull.edu.es>
2015-10-28 13:01:41 +0000

    Agregados unos cambios

--- lib/fraccion.rb
+++ lib/fraccion.rb
@@ -10,7 +10,8 @@ class Fraccion
    end
    
    def -(other)
-      return Fraccion.new(@num+other.num, @denum+other.denum) if @denum == other.denum
+      
+      return Fraccion.new(@num-other.num, @denum) if @denum == other.denum
       
       r=@num*other.denum
       s=other.num*@denum
@@ -21,18 +22,29 @@ class Fraccion
    end
    
    def +(other)
-       Fraccion.new(@a+other.a, @b+other.b)
+      
+      return Fraccion.new(@num+other.num, @denum) if @denum == other.denum
+      
+      r=@num*other.denum
+      s=other.num*@denum
+      t=@denum*other.denum
+      Fraccion.new(r+s, t)
+      
    end
    
    def *(other)
-       return Fraccion.new(@num*other, @denum) if other.is_a? Numeric
+      
+      return Fraccion.new(@num*other, @denum) if other.is_a? Numeric
        
-       Fraccion.new(@num*other.num, @denum*other.denum)
+      Fraccion.new(@num*other.num, @denum*other.denum)
+      
    end
    
    def /(other)
-       return Fraccion.new(@num, @denum*other) if other.is_a? Numeric
       
-       Fraccion.new(@num*other.denum, @denum*other.num)
+      return Fraccion.new(@num, @denum*other) if other.is_a? Numeric
+      
+      Fraccion.new(@num*other.denum, @denum*other.num)
+      
    end
 end
--- test/tc_fraccion.rb
+++ test/tc_fraccion.rb
@@ -2,34 +2,50 @@ require '../lib/fraccion.rb'
 require 'test/unit'
 
 class TestFraccion < Test::Unit::TestCase
-    attr_reader :f1, :f2, :rest, :mult, :div  
+    attr_reader :f1, :f2, :rest, :mult, :div, :sum, :sum1, :rest1, :mult1, :div1 
     
     def setup 
         @f1 = Fraccion.new(3,2)
         @f2 = Fraccion.new(7,5)
+        @f3 = Fraccion.new(3,8)
+        @f4 = Fraccion.new(7,8)
+        @sum = @f1+@f2
+        @sum1 = @f3+@f4
         @rest = @f2-@f1
+        @rest1 = @f4-@f3
         @mult = @f2*@f1
+        @mult1 = @f4*(3)
         @div = @f1/@f2
+        @div1 = @f3/(5)
         
     end 
     
     def test_suma
-         
+        assert_equal(sum.num, 29)
+        assert_equal(sum.denum,10)
+        assert_equal(sum1.num, 10)
+        assert_equal(sum1.denum, 8)
     end
 
     def test_resta
         assert_equal(rest.num, -1)
         assert_equal(rest.denum, 10)
+        assert_equal(rest1.num, 4)
+        assert_equal(rest1.denum, 8)
     end
     
     def test_multiplicacion
         assert_equal(mult.num, 21)
         assert_equal(mult.denum, 10)
+        assert_equal(mult1.num, 21)
+        assert_equal(mult1.denum, 8)
     end
     
     def test_division
         assert_equal(div.num, 15)
         assert_equal(div.denum, 14)
+        assert_equal(div1.num, 3)
+        assert_equal(div1.denum, 40)
     end
     
 end    
